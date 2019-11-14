eb07e15bd94191ebce0977d8fbdedf5f66feb9b7
YumaKimura <y.kimura.0608@gmail.com>
2019-11-14 21:20:58 +0900

    create events table

--- db/migrate/20191114120559_create_events.rb
+++ db/migrate/20191114120559_create_events.rb
@@ -1,6 +1,14 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.datetime :start_at
+      t.datetime :end_at
+      t.string :location
+      t.string :name
+      t.references :created_by, foreign_key: { to_table: :users }
+      t.integer :max_participant
+      t.string :budget
+      t.text :description
 
       t.timestamps
     end
