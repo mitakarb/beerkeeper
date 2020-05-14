f024ff9097cde9d03e4811de2a12942fa2fa80b0
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-05-14 20:49:47 +0900

    表示前にログイン状態を確認するように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  def authenticate
+    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate
+
   def new;end
 
   def create
