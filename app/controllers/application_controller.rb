db5b8fdb063b4f8e8be72e0a4d23dae31775b1f7
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-05-14 20:54:10 +0900

    current_user のヘルパーメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
   before_action :authenticate
 
   def authenticate
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
+
+  def current_user
+    @current_user
   end
 end
