266d3a1d819b5fef1ccfe2874832a0b0ee1a838c
Kuniaki Igarashi <igaiga@gmail.com>
2020-10-08 21:23:15 +0900

    Add spec/system/events_spec.rb

---
+++ spec/system/events_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe "events", type: :system do
+  it "enables me to create widgets" do
+    visit "/events" # /booksへHTTPメソッドGETでアクセス
+    expect(page).to have_text("イベント一覧") # 表示されたページに Books という文字があることを確認
+  end
+end
