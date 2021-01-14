6e6f2d72acacb4df3a30244be32c5d9ecd88bd28
tatsuosakurai <tatsuo.sakurai@gmail.com>
2021-01-14 21:22:28 +0900

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[create destroy]
   resources :events
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
