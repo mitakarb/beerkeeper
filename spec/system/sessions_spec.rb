4bf7240bd61986c879421ae796498493837e4d04
tatsuosakurai <tatsuo.sakurai@gmail.com>
2021-02-11 20:48:03 +0900

    Implement login feature

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "sessions", type: :system do
 
     it "enables us to login with email and password" do
       visit "/login"
-      fill_in :session_new_email, with: user.email
-      fill_in :session_new_password, with: user.password
-      click_on 'submit'
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on 'Login'
       expect(page).to have_content("ログインに成功しました")
     end
   end
