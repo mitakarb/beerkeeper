01ba365697d93ce31fcee24d296844e414cdb274
YutaGoto <you.goto.510@gmail.com>
2021-03-11 21:30:53 +0900

    add participation controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'events#index'
   get 'login', to: 'sessions#new'
   resources :sessions, only: %i[create destroy]
-  resources :events
+  resources :events do
+    resources :participations, only: %i[create destroy]
+  end
   resources :users
 end
