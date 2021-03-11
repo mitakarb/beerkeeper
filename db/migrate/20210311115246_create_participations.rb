84d149aab5900fc2359b55f6bb8307d3e4fad5b2
YutaGoto <you.goto.510@gmail.com>
2021-03-11 20:55:03 +0900

    add Participation model

---
+++ db/migrate/20210311115246_create_participations.rb
@@ -0,0 +1,10 @@
+class CreateParticipations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :participations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/participations.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :participation do
+    user { nil }
+    event { nil }
+  end
+end
