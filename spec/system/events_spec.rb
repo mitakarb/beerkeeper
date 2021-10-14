db9c3f4a3e7e8d449d51dd696b54dd5d8cb7fa94
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-10-14 20:42:02 +0900

    参加者がいないイベントの削除が成功するシステムスペックを書いた

--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe "events", type: :system do
     visit "/events" # /booksへHTTPメソッドGETでアクセス
     expect(page).to have_text("イベント一覧") # 表示されたページに Books という文字があることを確認
   end
+
+  describe "delete event" do
+    context "if participation exist" do
+      let(:user) { create(:user) }
+      let(:organizer) { create(:user) }
+      let(:event) { create(:event, organizer: organizer) }
+      before do
+        visit "/login"
+        fill_in :email, with: organizer.email
+        fill_in :password, with: organizer.password
+        click_on 'Login'
+        visit event_path(event)
+      end
+
+      it "successfully delete event" do
+        accept_confirm do
+          click_on 'このイベントを削除'
+        end
+      end
+    end
+  end
 end
