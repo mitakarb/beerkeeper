0caf0434726a48f00f835b2d4695003cbffc6e55
Yuta Goto <you.goto.510@gmail.com>
2022-05-12 11:16:26 +0000

    コンフリクトの解消

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,7 @@ module Beerkeeper
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-<<<<<<< HEAD
-
-=======
     config.i18n.default_locale = :ja
->>>>>>> 1e18010 (Merge branch 'master' into add-registration)
     config.generators do |g|
       g.template_engine :himl
     end
