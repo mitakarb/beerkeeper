8497cc08779d54d1ff284f13241e8df9cae2b40e
xnashiro <xnashiro@partner.cns.co.jp>
2023-05-18 20:50:44 +0900

    Move users#new to registrations#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'events#index'
   get 'login', to: 'sessions#new'
-  get 'signup', to: 'users#new'
+  get 'signup', to: 'registrations#new'
   resources :sessions, only: %i[create destroy]
   resources :events do
     resources :participations, only: %i[create destroy]
