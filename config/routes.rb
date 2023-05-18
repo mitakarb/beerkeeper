8dcd56055572a6e84447855d6c99af0c5d88e209
YutaGoto <you.goto.510@gmail.com>
2023-05-18 21:24:47 +0900

    fix routing, form with args

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'events#index'
   get 'login', to: 'sessions#new'
   get 'signup', to: 'registrations#new'
+  post 'signup', to: 'registrations#create'
+  
   resources :sessions, only: %i[create destroy]
   resources :events do
     resources :participations, only: %i[create destroy]
