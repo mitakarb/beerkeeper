fc5e8819b8a432c9b449e91909b34f73a6fa2980
YutaGoto <you.goto.510@gmail.com>
2023-09-16 12:05:00 +0900

    Alert Componentを追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Beerkeeper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.generators.test_framework = :rspec
 
     # Configuration for the application, engines, and railties goes here.
     #
