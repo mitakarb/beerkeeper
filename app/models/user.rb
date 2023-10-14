4a4e62b606a8293179f8ff043db93545852d4ad6
mizutaninaoki <applewizardplanet@gmail.com>
2023-10-14 16:08:50 +0900

    emailにユニークを確認するバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :participations, dependent: :nullify
 
   validates :name, presence: true
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
 
   def participatable?(event)
     !participations.find_by(event_id: event.id)
