5f577d68193d662d5bdd057dcbe185955aee501c
greendrop <greendrop.devel@gmail.com>
2024-02-08 20:54:19 +0900

    イベント名の必須入力のバリデーションを追加
    fix #655

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    describe 'name' do
+      let!(:event) { FactoryBot.build(:event, name: name) }
+
+      context '文字が入力されている場合' do
+        let!(:name) { 'イベント名' }
+
+        it { expect(event).to be_valid }
+      end
+
+      context '空文字の場合' do
+        let!(:name) { '' }
+
+        it { expect(event).to be_invalid }
+      end
+    end
+  end
 end
