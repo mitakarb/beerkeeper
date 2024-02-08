581f197c1450a35ac85a15e4a99e755ea60f22c7
a5-stable <sh07e1916@gmail.com>
2024-02-08 21:26:22 +0900

    add association & before_destroy callback

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
+  has_many :participations, dependent: :destroy
+
+  before_destroy do
+    throw(:abort)
+  end
 
   validates :start_at, presence: true
   validates :end_at, presence: true
