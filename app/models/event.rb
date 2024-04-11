a13f36d157b7d2f54736e1a6a3f4f2c0263c2818
greendrop <greendrop.devel@gmail.com>
2024-04-11 21:35:10 +0900

    開始日時・終了日時の必須バリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
 
+  validates :start_at, presence: true
+  validates :end_at, presence: true
   validates :name, presence: true
 
   def organizer?(user)
