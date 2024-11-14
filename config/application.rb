af4f8bab396bdee5d93f349fd77be339e45ec81f
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2024-11-14 20:52:29 +0900

    bin/rails app:update

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,11 @@ module Beerkeeper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-    config.generators.test_framework = :rspec
-
+config.generators.test_framework = :rspec
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
