ac88f61ba13a012c4c45fa4aebc4af9d901ef2c5
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-11 21:32:35 +0900

    add unique constrait to event_cancellations

--- db/migrate/20251211121503_create_event_cancellations.rb
+++ db/migrate/20251211121503_create_event_cancellations.rb
@@ -1,7 +1,7 @@
 class CreateEventCancellations < ActiveRecord::Migration[8.1]
   def change
     create_table :event_cancellations do |t|
-      t.belongs_to :event, null: false, foreign_key: true
+      t.belongs_to :event, null: false, foreign_key: true, index: { unique: true }
       t.string :reason
 
       t.timestamps
