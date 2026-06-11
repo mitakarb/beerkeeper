2582b7e6ef7ae950d2560e879584dea4babbea0d
jellyfish700 <shiho.asano@gmail.com>
2026-06-11 21:01:58 +0900

    キャンセル時のリダイレクト先を一覧画面に変更

--- app/controllers/events/cancellations_controller.rb
+++ app/controllers/events/cancellations_controller.rb
@@ -3,6 +3,6 @@ class Events::CancellationsController < ApplicationController
     @event = Event.find(params[:event_id])
     @event.cancel(params.dig(:cancellation, :reason))
 
-    redirect_to @event, notice: 'イベントをキャンセルしました'
+    redirect_to root_path, notice: 'イベントをキャンセルしました'
   end
 end
